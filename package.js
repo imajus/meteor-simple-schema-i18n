9c0977cc94f6714a43672318f6608c2b23266af0
Denis Perov <denis.perov@gmail.com>
2017-10-18 20:02:21 +0300

    first commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'simple-schema-messages-i18n',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Support SimpleSchema messages translations using tap:i18n',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  'simpl-schema': '0.3.1'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.5.2.2');
+  api.use('ecmascript', 'tap:i18n');
+  api.mainModule('ssm-i18n.client.js', 'client');
+  api.mainModule('ssm-i18n.server.js', 'server');
+});
---
+++ ssm-i18n.client.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See simple-schema-errors-i18n-tests.js for an example of importing.
+export const name = 'simple-schema-errors-i18n';
